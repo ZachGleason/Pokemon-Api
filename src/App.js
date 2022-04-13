779464639cd88c69e841d0ec9e96241a1e49071b
Zach <zgleason37@gmail.com>
2022-04-13 10:17:49 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+const App = (props) => {
+  const [people, setPeople] = useState([]);
+
+  // useEffect(() => {
+  //   alert("HELLOOO ")
+  // }, [])
+  // This effect is called once whenever a webpage is loaded
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then(response => response.json())
+    .then(response => setPeople(response.results))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {people.map((person, index)=>{
+            return (<div key={index} >
+              <li className="pokemons">{person.name}</li>
+              </div>)
+        })}
     </div>
-  );
+);
+
 }
 
 export default App;
